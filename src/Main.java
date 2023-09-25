6ff414621d86c2ff8d11808cdfcd1db5a0e11dbd
Paulo Carabuena <pawekzcarabuena@gmail.com>
2023-09-25 22:25:38 +0800

    Revision: added another sample edit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Sample for GIT commit and push");
-        System.out.print("Paulo Carabuena");
+        System.out.print("Paulo Carabuena the Dinosaur");
     }
 }
